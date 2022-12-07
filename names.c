ab79cb614c917ae97af0bab70a7714e0d738ff7d
leviathan97 <92945267+leviathan97@users.noreply.github.com>
2022-12-07 17:01:04 +0000

    Wed, Dec 7, 2022, 5:01 PM +00:00

---
+++ names.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int names[] = {"Bill", "Charlie", "fred", "george", "ginny", "percy", "ron"};
+
+    for(i = 0; i < 7; i++)
+    
+}
