bda6a84dc35dbe7d8e403f08d2f58db93e03ac1f
leviathan97 <92945267+leviathan97@users.noreply.github.com>
2022-12-21 18:08:29 +0000

    Wed, Dec 21, 2022, 6:08 PM +00:00

---
+++ points.py
@@ -0,0 +1,11 @@
+
+points = int(input("How many point did you take?: "))
+
+if points < 2:
+    print("you took less points than me")
+
+elif points > 2:
+    print("you took more points than me")
+
+else:
+    print("we took same points")
